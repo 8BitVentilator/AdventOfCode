298bc2e0a6b2de458b666568a153fc05b1e39e67
8BitVentilator <33177269+8BitVentilator@users.noreply.github.com>
2024-12-02 10:51:53 +0100

    Refactoring Runner

--- AdventOfCode.App/PuzzleResult.cs
+++ AdventOfCode.App/PuzzleResult.cs
@@ -1,3 +1,3 @@
-﻿namespace AdventOfCode;
+﻿namespace AdventOfCode.App;
 
 public record PuzzleResult(string Name, object? PartOne, long? TimePartOne, object? PartTwo, long? TimePartTwo);
--- AdventOfCode.App/Runner.cs
+++ AdventOfCode.App/Runner.cs
@@ -3,18 +3,9 @@ using System.Reflection;
 using System.Text.RegularExpressions;
 
 namespace AdventOfCode.App;
+
 internal class Runner(Settings settings)
 {
-    private string RegexPattern
-        => $"Year{settings.Year}.Day({string.Join('|', settings.Days.Select(x => x.ToString("00")))})";
-
-    private IEnumerable<IPuzzle> Puzzles
-        => Assembly.GetAssembly(typeof(IPuzzle))!
-                   .GetTypes()
-                   .Where(type => type.IsClass && type.IsAssignableTo(typeof(IPuzzle)) && Regex.IsMatch(type.FullName!, RegexPattern))
-                   .OrderBy(type => type.FullName)
-                   .Select(type => (IPuzzle)Activator.CreateInstance(type)!);
-
     public IEnumerable<PuzzleResult> Run()
     {
         static int[] YearAndDay(string @namespace) => Regex.Matches(@namespace, "[0-9]+").Select(match => int.Parse(match.Value)).ToArray();
@@ -24,23 +15,30 @@ internal class Runner(Settings settings)
             return $@"Year{yearAndDay[0]}\Day{yearAndDay[1]:00}";
         }
 
-
-
-        var sw = new Stopwatch();
         foreach (var puzzle in Puzzles)
         {
             var input = File.ReadAllLines($@"{Folder(puzzle.GetType().FullName!)}\input.txt");
-            var partOne = RunPuzzlePart(() => puzzle.PartOne(input));
-            var partTwo = RunPuzzlePart(() => puzzle.PartTwo(input));
+            var (partOneresult, partOneElapsedMilliseconds) = RunPuzzlePart(() => puzzle.PartOne(input));
+            var (partTworesult, partTwoElapsedMilliseconds) = RunPuzzlePart(() => puzzle.PartTwo(input));
 
             yield return new PuzzleResult(
-                puzzle.GetType().FullName!,
-                partOne.result, partOne.elapsedMilliseconds,
-                partTwo.result, partTwo.elapsedMilliseconds);
+                puzzle.GetType().Namespace ?? "",
+                partOneresult, partOneElapsedMilliseconds,
+                partTworesult, partTwoElapsedMilliseconds
+            );
         }
     }
+    private string RegexPattern
+    => $"Year{settings.Year}.Day({string.Join('|', settings.Days.Select(x => x.ToString("00")))})";
 
-    private (object? result, long? elapsedMilliseconds) RunPuzzlePart(Func<object> part)
+    private IEnumerable<IPuzzle> Puzzles
+        => Assembly.GetAssembly(typeof(IPuzzle))!
+                   .GetTypes()
+                   .Where(type => type.IsClass && type.IsAssignableTo(typeof(IPuzzle)) && Regex.IsMatch(type.FullName!, RegexPattern))
+                   .OrderBy(type => type.FullName)
+                   .Select(type => (IPuzzle)Activator.CreateInstance(type)!);
+
+    private static (object? result, long? elapsedMilliseconds) RunPuzzlePart(Func<object> part)
     {
         try
         {
