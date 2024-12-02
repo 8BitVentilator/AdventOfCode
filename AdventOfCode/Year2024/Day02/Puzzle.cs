1bb1753aea4995da3dcfb3b3e429bd455082df1f
8BitVentilator <33177269+8BitVentilator@users.noreply.github.com>
2024-12-02 14:22:06 +0100

    Change levels in report in Year 2024 Day 02

--- AdventOfCode/Year2024/Day02/Puzzle.cs
+++ AdventOfCode/Year2024/Day02/Puzzle.cs
@@ -15,20 +15,20 @@ public class Puzzle : IPuzzle
         => input.Select(report => Regex.Matches(report, @"\d+"))
                 .Select(levels => levels.Select(level => Convert.ToInt32(level.Value)));
 
-    private static IEnumerable<IEnumerable<int>> TolerantReports(IEnumerable<int> levels)
-        => Enumerable.Range(0, levels.Count())
-                     .Select(x => levels.Where((_, i) => i != x));
+    private static IEnumerable<IEnumerable<int>> TolerantReports(IEnumerable<int> report)
+        => Enumerable.Range(0, report.Count())
+                     .Select(x => report.Where((_, i) => i != x));
 
-    private static bool IsSafe(IEnumerable<int> levels)
+    private static bool IsSafe(IEnumerable<int> report)
     {
-        static IEnumerable<int> AdjacentDifferences(IEnumerable<int> levels)
-            => Enumerable.Zip(levels, levels.Skip(1), (left, right) => right - left);
+        static IEnumerable<int> AdjacentDifferences(IEnumerable<int> report)
+            => Enumerable.Zip(report, report.Skip(1), (left, right) => right - left);
 
         static bool IsPositive(int difference) => difference > 0;
         static bool IsNegative(int difference) => difference < 0;
         static bool IsLessOrEqualThree(int difference) => Math.Abs(difference) <= 3;
 
-        var adjacentDifferences = AdjacentDifferences(levels).ToList();
+        var adjacentDifferences = AdjacentDifferences(report).ToList();
         return adjacentDifferences.All(x => IsPositive(x) && IsLessOrEqualThree(x))
             || adjacentDifferences.All(x => IsNegative(x) && IsLessOrEqualThree(x));
     }
