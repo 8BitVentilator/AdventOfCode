1b73e767356ea1ae0eca8ccc3c000795fbb828b4
8BitVentilator <33177269+8BitVentilator@users.noreply.github.com>
2024-12-03 10:32:50 +0100

    Refactoring Day 3 Year 2024

--- AdventOfCode/Year2024/Day03/Puzzle.cs
+++ AdventOfCode/Year2024/Day03/Puzzle.cs
@@ -10,20 +10,19 @@ public partial class Puzzle : IPuzzle
     [GeneratedRegex(@"(?<name>do)\(\)|(?<name>don't)\(\)|(?<name>mul)\((?<x>\d+),(?<y>\d+)\)")]
     private static partial Regex PartTwoRegex();
 
-    public object PartOne(string[] input) => Process(Instructions(input, PartOneRegex()));
+    public object PartOne(string[] input) => Process(input, PartOneRegex());
 
-    public object PartTwo(string[] input) => Process(Instructions(input, PartTwoRegex()));
+    public object PartTwo(string[] input) => Process(input, PartTwoRegex());
 
-    private static IEnumerable<Match> Instructions(string[] input, Regex regex)
-        => input.SelectMany(line => regex.Matches(line));
-
-    private static int Process(IEnumerable<Match> instructions)
-        => instructions.Aggregate((process: true, result: 0),
-            (acc, instruction) => (instruction.Groups["name"].Value, acc.process) switch
-            {
-                ("do", _) => (true, acc.result),
-                ("don't", _) => (false, acc.result),
-                ("mul", true) => (true, acc.result += int.Parse(instruction.Groups["x"].Value) * int.Parse(instruction.Groups["y"].Value)),
-                _ => acc
-            }).result;
+    private static int Process(string[] input, Regex regex)
+        => input.SelectMany(line => regex.Matches(line))
+                .Aggregate((process: true, result: 0),
+                    (acc, instruction) => (instruction.Groups["name"].Value, acc.process) switch
+                    {
+                        ("do", _) => (true, acc.result),
+                        ("don't", _) => (false, acc.result),
+                        ("mul", true) => (true, acc.result += int.Parse(instruction.Groups["x"].Value) * int.Parse(instruction.Groups["y"].Value)),
+                        _ => acc
+                    })
+                .result;
 }
